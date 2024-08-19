3b75477211513918f0a8dc6e55b91e09ec86d78f
Marcio dos Santos Junior <marciojunior.gm@gmail.com>
2024-08-19 11:49:55 -0300

    feat: add heap verification in main

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import argparse
+from typing import List
 import random
+
+from utils.heap import Heap
 
 from methods.p_ways import PWays
 from methods.polyphasic import Polyphasic
 from methods.cascade import Cascade
 
+def get_valid_registers(sorted_sequences: List[List[int]], r) -> List[int]:
+    valid_registers = []
+    for i in range(r):
+        valid_registers.extend(sorted_sequences[i])
+    return valid_registers
+
 if __name__ == "__main__":
     method = input()
     m, k, r, n = map(int, input().split(' '))
     registers = list(map(int, input().split(' ')))[:n]
+
+    # if the heap result returns more sorted sequences than expected, get the valid registers
+    heap = Heap(main_memory_size=m, registers=registers)
+    sorted_sequences: List[List[int]] = heap.sort()
+    if len(sorted_sequences) != r:
+        registers:List[int] = get_valid_registers(sorted_sequences, r)
 
     if len(registers) == 0:
         registers = [random.randint(0,100) for _ in range(n)]

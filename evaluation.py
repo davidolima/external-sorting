1afe5c06a8c85643fcc11fe7d4db30f782890ec1
David <david@g15>
2024-08-22 12:10:42 -0300

    feat: Accept sequence for evaluation

--- evaluation.py
+++ evaluation.py
@@ -64,6 +64,7 @@ class Evaluator():
 
         title = f"{x_label} x {y_label}" if title is None else title
         print("[!] Generating graph...", end=' ')
+
         plt.plot(x, y)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
@@ -130,28 +131,28 @@ class Evaluator():
 
         return alpha
 
-    def test_alpha(self, m: int, k: int, r_lower_limit: int, r_upper_limit: int, save_results: bool = False) -> List[float]:
+    def test_alpha(self, m: int, k: int, r_values: List[int], save_results: bool = False) -> List[float]:
         results = []
 
         start_time = time.perf_counter()
-        for i in tqdm(range(r_lower_limit, r_upper_limit)):
+        for i in tqdm(r_values):
             result = self.run_with_r_sequences(r=i, m=m, k=k)
             results.append(result)
         end_time = time.perf_counter()
 
-        print(f"[!] Ran {r_upper_limit - r_lower_limit} tests in {end_time - start_time} seconds.")
+        print(f"[!] Ran {len(r_values)} tests in {end_time - start_time} seconds.")
 
         if save_results:
             assert self.output_path is not None, "You need to define an output dir to be able to save the results."
             if os.path.isdir(self.output_path):
-                fpath = os.path.join(self.output_path, f"alpha_test_{self.get_alg_name()}_m{m}_k{k}_ru{r_upper_limit}.csv")
+                fpath = os.path.join(self.output_path, f"alpha_test_{self.get_alg_name()}_m{m}_k{k}.csv")
             else:
                 fpath = self.output_path
 
             print("[!] Saving results...", end=' ')
             with open(fpath, 'w+') as f:
-                for i in range(len(results)):
-                    f.write(f"{i + r_lower_limit}, {results[i]:.2f}\n")
+                for i in r_values:
+                    f.write(f"{i}, {results[i]:.2f}\n")
 
             print(f"Done. Results saved to `{fpath}`.")
 
@@ -160,22 +161,19 @@ class Evaluator():
     def test_k(
         self,
         m: int,
-        k_lower_limit:int,
-        k_upper_limit:int,
-        r_lower_limit:int,
-        r_upper_limit: int,
+        k_values: List[int],
+        r_values: List[int],
         save_results: bool = False,
         generate_graph: bool = True
     ) -> List[float]:
         start_time = time.perf_counter()
-        interval = list(range(k_lower_limit, k_upper_limit+1))
-        for i in tqdm(interval):
-            alphas = self.test_alpha(m=m, k=i, r_lower_limit=r_lower_limit, r_upper_limit=r_upper_limit, save_results = False)
+        for i in tqdm(k_values):
+            alphas = self.test_alpha(m=m, k=i, r_values=r_values, save_results = False)
 
             if save_results:
                 assert self.output_path is not None, "You need to define an output dir to be able to save the results."
                 print("[!] Saving results...", end=' ')
-                fpath  = os.path.join(self.output_path, f"m_test_{self.get_alg_name()}_m{m}_k{i}_ru{r_upper_limit}")
+                fpath  = os.path.join(self.output_path, f"m_test_{self.get_alg_name()}_m{m}_k{i}")
 
                 with open(fpath + ".csv", 'w+') as f:
                     for i in range(len(alphas)):
@@ -186,65 +184,67 @@ class Evaluator():
 
             if generate_graph:
                 assert self.output_path is not None, "You need to define an output dir to be able to save the results."
-                fpath  = os.path.join(self.output_path, f"m_test_{self.get_alg_name()}_m{m}_k{i}_ru{r_upper_limit}")
+                fpath  = os.path.join(self.output_path, f"m_test_{self.get_alg_name()}_m{m}_k{i}")
                 plt.style.use('ggplot')
                 evaluator.generate_graph(
-                    x = list(range(int(args.r_lower_limit),int(args.r_upper_limit))),
+                    x = r_values,
                     y = alphas,
                     x_label = r"Nº Sequencias iniciais ($r$)",
                     y_label = r"Taxa de processamento ($\alpha$)",
-                    title=f"m={m}",
+                    title=self.get_alg_name(),
                     fpath=fpath + '.png',
-                    legend=[f"k={x}" for x in interval],
+                    legend=[f"k={x}" for x in k_values],
                 )
 
 
         end_time = time.perf_counter()
-        print(f"[!] Ran {k_upper_limit - k_lower_limit} tests in {end_time - start_time} seconds.")
-
+        print(f"[!] Ran {len(k_values)} tests in {end_time - start_time} seconds.")
 
         return alphas
 
     def test_k_for_all(
         self,
         m: int,
-        k_lower_limit: int,
-        k_upper_limit: int,
-        r_lower_limit: int,
-        r_upper_limit: int,
+        k_values: List[int],
+        r_values: List[int],
         algorithms: Tuple[Literal["B"], Literal["P"], Literal["C"]] = ("B", "P", "C"),
         save_results: bool = True,
         generate_graph: bool = True,
     ):
         for alg in algorithms:
+            plt.clf()
             self.algoritmo = alg
             self.test_k(
                 m=m,
-                k_lower_limit=k_lower_limit,
-                k_upper_limit=k_upper_limit,
-                r_lower_limit=r_lower_limit,
-                r_upper_limit=r_upper_limit,
+                k_values=k_values,
+                r_values=r_values,
                 save_results=save_results,
                 generate_graph=generate_graph
             )
 
     def test_beta(
             self,
-            m_lower_limit: int,
-            m_upper_limit: int,
-            N_OF_REGS: int = 5000,
+            m_values: List[int],
+            N_OF_REGS: int = 500,
             save_results: bool = False,
             generate_graph: bool = False,
+            fixed_seq: bool = False,
     ) -> None:
         betas = []
-        interval = list(range(m_lower_limit, m_upper_limit+1))
-        for m in tqdm(interval):
+        heap_results = []
+        if fixed_seq:
             regs = Evaluator._generate_random_sequence(N_OF_REGS)
+
+        print("Fixed sequence:", regs)
+        for m in tqdm(m_values):
+            if not fixed_seq:
+                regs = Evaluator._generate_random_sequence(N_OF_REGS)
             sorted_seqs = Heap(
                 main_memory_size=m,
                 registers=regs
             ).sort()
 
+            heap_results.append(sorted_seqs)
             betas.append(
                 beta(m, len(sorted_seqs), N_OF_REGS, depth=0)
             )
@@ -252,26 +252,26 @@ class Evaluator():
         if save_results:
             assert self.output_path is not None, "You need to define an output dir to be able to save the results."
             print("[!] Saving results...", end=' ')
-            fpath  = os.path.join(self.output_path, f"beta_test_m{m}")
+            fpath  = os.path.join(self.output_path, f"beta_test_m{m}" if not fixed_seq else f"beta_test_m{m}_fixed")
 
             with open(fpath + ".csv", 'w+') as f:
                 for i in range(len(betas)):
-                    f.write(f"{i}, {betas[i]:.2f}\n")
+                    f.write(f"{i}, {betas[i]:.2f} , {heap_results[i]}\n")
 
             print(f"Results of k={i} saved to `{fpath}.csv`.", end=' ')
         print()
 
         if generate_graph:
             assert self.output_path is not None, "You need to define an output dir to be able to save the results."
-            fpath  = os.path.join(self.output_path, f"beta_test_m{m}")
+            fpath  = os.path.join(self.output_path, f"beta_test_m{m}" if not fixed_seq else f"beta_test_m{m}_fixed")
             plt.style.use('ggplot')
             evaluator.generate_graph(
-                x = interval,
+                x = m_values,
                 y = betas,
                 x_label = r"Tam. da memória principal ($m$)",
                 y_label = r"$\beta$",
                 fpath=fpath + '.png',
-                legend=[f"m={x}" for x in interval],
+                legend=[f"m={x}" for x in m_values],
             )
 
 
@@ -281,10 +281,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--algorithms",        type=str, default='C')
     parser.add_argument("-m", "--main-memory-size",  type=int, default=3)
-    parser.add_argument("-kl", "--k-lower-limit",    type=int, default=3)
-    parser.add_argument("-ku", "--k-upper-limit",    type=int, default=6)
-    parser.add_argument("-rl", "--r-lower-limit",    type=int, default=3)
-    parser.add_argument("-ru", "--r-upper-limit",    type=int, default=100)
     parser.add_argument("-o", "--output",            type=str, default="results/")
     args = parser.parse_args()
 
@@ -293,20 +289,27 @@ if __name__ == "__main__":
         output_path=args.output
     )
 
+    # r_values = []
+    # for i in range(1,11): # R = {i x j <= 5000 | i = 1, 2, ... , 10; j = 10, 20, ... , 1000}
+    #     for j in range(10,1001,10):
+    #         if i*j <= 5000 and not i*j in r_values:
+    #             r_values.append(i*j)
+    # r_values = sorted(r_values)
+
     # evaluator.test_k_for_all(
     #     algorithms=("B", "P", "C"),
     #     m=args.main_memory_size,
-    #     k_lower_limit=args.k_lower_limit,
-    #     k_upper_limit=args.k_upper_limit,
-    #     r_lower_limit=args.r_lower_limit,
-    #     r_upper_limit=args.r_upper_limit,
+    #     k_values=list(range(4,13,2)),
+    #     r_values=r_values,
     #     save_results=True,
     #     generate_graph=True
     # )
 
+    m_values = [1] + [3*(5*i) for i in range(1,5)]
     evaluator.test_beta(
-        m_lower_limit=3,
-        m_upper_limit=3000,
+        m_values=list(range(3,61)),#m_values,
         save_results=True,
-        generate_graph=True
+        generate_graph=True,
+        N_OF_REGS=500,
+        fixed_seq=True,
     )

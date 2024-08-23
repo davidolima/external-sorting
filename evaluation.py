d86818b294eb310ea8e7479bde748dac16dab88e
David <david@g15>
2024-08-23 15:46:55 -0300

    feat: Selection of which metric to evaluate

--- evaluation.py
+++ evaluation.py
@@ -55,7 +55,15 @@ class Evaluator():
             case 'C': return "Cascade"
             case _: raise Exception("Unreachable.")
 
-    def generate_graph(self, x, y, x_label, y_label, title = None, fpath = None, legend = None) -> None:
+    def generate_graph(
+        self,
+        x, y,
+        x_label, y_label,
+        title = None,
+        fpath = None,
+        legend = None,
+        y_lim: Union[Tuple[int,int],None] = None
+    ) -> None:
         assert self.output_path is not None, "You need to set `self.output_path` to generate a graph."
 
         if fpath is None:
@@ -69,6 +77,9 @@ class Evaluator():
         plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.title(title)
+        if y_lim is not None:
+            ax = plt.gca()
+            ax.set_ylim([y_lim[0], y_lim[1]])
         if legend is not None:
             plt.legend(legend)
         plt.savefig(fpath)
@@ -88,12 +99,6 @@ class Evaluator():
                 save_results=False,
                 is_inputing_sorted_sequences=True
             )
-
-            #for i in range(len(initial_sequences)):
-                #file_index: int = i % alg._num_input_files
-                #sorted_sequence = sorted(initial_sequences[i])
-                #alg._files[file_index].append(sorted_sequence)
-                #alg._index_input_files.add(file_index)
 
         elif self.algoritmo == 'P':
             initial_sequences = Evaluator._generate_ordered_runs(size=r, main_memory_size=m)
@@ -122,14 +127,16 @@ class Evaluator():
         try:
             alpha = alg.sort()
             togglePrint() # Re-enable printing
+            return alpha
         except Exception as e:
             # In case of error, rerun the algorithm with
             # printing enabled while using same cfg
             # to understand what happened.
             togglePrint() # Re-enable printing
             alg.sort()
+            return -1
 
-        return alpha
+        assert False, "Unreachable"
 
     def test_alpha(self, m: int, k: int, r_values: List[int], save_results: bool = False) -> List[float]:
         results = []
@@ -231,11 +238,12 @@ class Evaluator():
             fixed_seq: bool = False,
     ) -> None:
         betas = []
-        heap_results = []
+        heap_results_len = []
+        avg_seq_size = []
         if fixed_seq:
             regs = Evaluator._generate_random_sequence(N_OF_REGS)
 
-        print("Fixed sequence:", regs)
+        #print("Fixed sequence:", regs)
         for m in tqdm(m_values):
             if not fixed_seq:
                 regs = Evaluator._generate_random_sequence(N_OF_REGS)
@@ -244,7 +252,8 @@ class Evaluator():
                 registers=regs
             ).sort()
 
-            heap_results.append(sorted_seqs)
+            heap_results_len.append(len(sorted_seqs))
+            avg_seq_size.append(sum([len(x) for x in sorted_seqs])/len(sorted_seqs))
             betas.append(
                 beta(m, len(sorted_seqs), N_OF_REGS, depth=0)
             )
@@ -256,7 +265,7 @@ class Evaluator():
 
             with open(fpath + ".csv", 'w+') as f:
                 for i in range(len(betas)):
-                    f.write(f"{i}, {betas[i]:.2f} , {heap_results[i]}\n")
+                    f.write(f"{m_values[i]}, {betas[i]:.2f}\n")
 
             print(f"Results of k={i} saved to `{fpath}.csv`.", end=' ')
         print()
@@ -265,13 +274,37 @@ class Evaluator():
             assert self.output_path is not None, "You need to define an output dir to be able to save the results."
             fpath  = os.path.join(self.output_path, f"beta_test_m{m}" if not fixed_seq else f"beta_test_m{m}_fixed")
             plt.style.use('ggplot')
-            evaluator.generate_graph(
-                x = m_values,
-                y = betas,
+
+            evaluator.generate_graph( # Gráfico para o beta
+                x       = m_values,
+                y       = betas,
                 x_label = r"Tam. da memória principal ($m$)",
                 y_label = r"$\beta$",
-                fpath=fpath + '.png',
-                legend=[f"m={x}" for x in m_values],
+                y_lim   = (0, 4),
+                fpath   = fpath + '.png',
+                legend  = [f"m={x}" for x in m_values],
+            )
+
+            fpath  = os.path.join(self.output_path, f"beta_test_m{m}" if not fixed_seq else f"beta_test_m{m}_n_seqs_fixed")
+            plt.clf()
+            evaluator.generate_graph( # Gráfico para o número de sequências
+                x       = m_values,
+                y       = heap_results_len,
+                x_label = r"Tam. da memória principal ($m$)",
+                y_label = r"Qtd. Sequências",
+                fpath   = fpath + '.png',
+                legend  = [f"m={x}" for x in m_values],
+            )
+
+            fpath  = os.path.join(self.output_path, f"beta_test_m{m}" if not fixed_seq else f"beta_test_m{m}_avg_seq_size_fixed")
+            plt.clf()
+            evaluator.generate_graph( # Gráfico para o tam. médio de seqs
+                x       = m_values,
+                y       = avg_seq_size,
+                x_label = r"Tam. da memória principal ($m$)",
+                y_label = r"Tam. médio das Sequências",
+                fpath   = fpath + '.png',
+                legend  = [f"m={x}" for x in m_values],
             )
 
 
@@ -282,6 +315,8 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--algorithms",        type=str, default='C')
     parser.add_argument("-m", "--main-memory-size",  type=int, default=3)
     parser.add_argument("-o", "--output",            type=str, default="results/")
+    parser.add_argument("-alpha", "--test-alpha",    action="store_true")
+    parser.add_argument("-beta", "--test-beta",      action="store_true")
     args = parser.parse_args()
 
     evaluator = Evaluator(
@@ -289,27 +324,38 @@ if __name__ == "__main__":
         output_path=args.output
     )
 
-    # r_values = []
-    # for i in range(1,11): # R = {i x j <= 5000 | i = 1, 2, ... , 10; j = 10, 20, ... , 1000}
-    #     for j in range(10,1001,10):
-    #         if i*j <= 5000 and not i*j in r_values:
-    #             r_values.append(i*j)
-    # r_values = sorted(r_values)
+    if (not args.test_alpha and not args.test_beta):
+        print("[!] No argument provided (-alpha or -beta).")
+        parser.print_usage()
+        quit(1)
 
-    # evaluator.test_k_for_all(
-    #     algorithms=("B", "P", "C"),
-    #     m=args.main_memory_size,
-    #     k_values=list(range(4,13,2)),
-    #     r_values=r_values,
-    #     save_results=True,
-    #     generate_graph=True
-    # )
+    if args.test_alpha:
+        r_values = []
+        for i in range(1,11): # R = {i x j <= 5000 | i = 1, 2, ... , 10; j = 10, 20, ... , 1000}
+            for j in range(10,1001,10):
+                if i*j <= 5000 and not i*j in r_values:
+                    r_values.append(i*j)
+        r_values = sorted(r_values)
 
-    m_values = [1] + [3*(5*i) for i in range(1,5)]
-    evaluator.test_beta(
-        m_values=list(range(3,61)),#m_values,
-        save_results=True,
-        generate_graph=True,
-        N_OF_REGS=500,
-        fixed_seq=True,
-    )
+        evaluator.test_k_for_all(
+            algorithms=("B", "P", "C"),
+            m=args.main_memory_size,
+            k_values=list(range(4,13,2)),
+            r_values=r_values,
+            save_results=True,
+            generate_graph=True
+        )
+
+    if args.test_beta:
+        m_values = [3] + [3*(5*i) for i in range(1,5)]
+        print("m_values:", m_values)
+        evaluator.test_beta(
+            m_values=m_values,
+            save_results=True,
+            generate_graph=True,
+            N_OF_REGS=10_000,
+            fixed_seq=True,
+        )
+
+    print("[!] All done!")
+    quit(0)
